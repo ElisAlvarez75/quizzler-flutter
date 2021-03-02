59dcf3400c034fd8c44e5fe986c815bfa4f698b5
Elis Alvarez <elisalvarez@msn.com>
2021-03-02 11:27:42 +0100

    App finish

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -46,14 +46,14 @@ class QuizBrain {
   }
 
   bool isFinished() {
-    if (_questionNumber == _questionBank.length) {
+    if (_questionNumber == _questionBank.length - 1) {
       return true;
     } else {
       return false;
     }
   }
 
-//TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
-
-//TODO: Step 4 Part B - Create a reset() method here that sets the questionNumber back to 0.
+  void reset() {
+    _questionNumber = 0;
+  }
 }
